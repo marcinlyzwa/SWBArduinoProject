8e01450788cdd0f5b40185797b1b2972c6f2f246
Tomasz Marmołowski <tomasz@marmolowski.poland>
2012-06-12 19:12:17 +0200

    Obsługa serva po stronie Arduino
    Dodatkowo zmieniłem kodowanie "co zostało" na UTF-8

--- Projekt/Projekt.ino
+++ Projekt/Projekt.ino
@@ -13,7 +13,7 @@ void guzik() {
 
 void setup() {
   Serial.begin(9600);
-  
+
   pinMode(11, OUTPUT);
   servo.attach(11);
   servo.write(0);
@@ -45,7 +45,7 @@ void loop() {
     startNewGame();
   } 
   else if (isSerwoOrder(napis)) {
-    
+    servo.write(extractServoValue(napis));
   } 
   else {
     lcd.setCursor(0,1);
@@ -100,13 +100,38 @@ void readLine(char * napis) {
 }
 
 boolean isGameOver(char * napis) {
+  
   char napisOver[4] = {
-    'O', 'V', 'E', 'R'      };
+    'O', 'V', 'E', 'R'  };
+    
   boolean over = true;
-
-  for (int i = 0; i<4; i++)
+  for (int i = 0; i<4; ++i)
     over = over && napis[i] == napisOver[i];
 
   return over;
 }
 
+boolean isSerwoOrder(char * napis) {
+  
+  char napisSerwo[6] = {
+    'S', 'E', 'R', 'V', 'O', ' ' };
+    
+  boolean servo = true;
+  for (int i = 0; i<6; ++i)
+    servo = servo && napis[i] == napisSerwo[i];
+
+  return servo;
+}
+
+int extractServoValue(char * napis) {
+  int value = 0;
+  
+  int i = 6;
+  while (napis[i] >= '0' && napis[i] <= '9') {
+    value *= 10;
+    value += napis[i] - '0';
+    ++i;
+  }
+  
+  return value;
+}
